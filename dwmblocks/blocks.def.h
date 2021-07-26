13dd81075c4d4ffa9f7fe5f742e01722aadac9d4
EulerSniper69 <69073098+EulerSniper69@users.noreply.github.com>
2021-07-26 18:46:31 +0200

    Add files via upload

---
+++ dwmblocks/blocks.def.h
@@ -0,0 +1,21 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
+
+    {" 🐧 ", "/home/helico/dwm/dwmblocks-distrotube/scripts/kernel",		    360,		        2},
+
+	{" 🔺 ", "/home/helico/dwm/dwmblocks-distrotube/scripts/upt",		        60,		            2},
+
+	{" 📦 ", "/home/helico/dwm/dwmblocks-distrotube/scripts/pacupdate",		360,		        9},
+	
+    {"  💻", "/home/helico/dwm/dwmblocks-distrotube/scripts/cpu",			1,		            0},
+	{" 💾 ", "/home/helico/dwm/dwmblocks-distrotube/scripts/memory",	        1,		            0},
+
+	{" 🔊 ", "/home/helico/dwm/dwmblocks-distrotube/scripts/volume",			2,		            0},
+
+	{" 🕑 ", "/home/helico/dwm/dwmblocks-distrotube/scripts/clock",			5,		            0},
+
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
